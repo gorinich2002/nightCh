2f0867adc7ca4b3f6e5f7745a06dd46e8da33245
Мясников Егор Евгеньевич <Myasnikov_EE@radius.belgorod.com>
2022-07-04 10:57:02 +0300

    fix pacages configs for import

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log(1)
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('postgres://admin:POSTGRES_PASSWORD.com:5432/board');
+
+(async ()=>{
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+})()
